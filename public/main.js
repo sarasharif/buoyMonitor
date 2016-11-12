1290726ac932c47dc46a7f0231b368e5fb4bd8b9
Sara <sara@sharif.com>
2016-11-12 13:37:50 -0800

    makes ajax calls to server and receives fake data

--- public/main.js
+++ public/main.js
@@ -13,9 +13,23 @@ $(document).ready(function(){
 function showAllBuoys() {
   $(".allBuoys").removeClass("hidden").html('<h1>....loading buoys within radius...</h1>');
   $(".favBuoys").addClass("hidden");
+  getAllBuoys();
 }
 
 function showFavBuoys() {
   $(".favBuoys").removeClass("hidden").html('<h1>....loading your favorite buoys....</h1>');
   $(".allBuoys").addClass("hidden");
+  getFavBuoys();
+}
+
+function getAllBuoys() {
+  $.get("/api/allBuoys", function (data) {
+    console.log(data);
+  });
+}
+
+function getFavBuoys() {
+  $.get("/api/favBuoys", function (data) {
+    console.log(data);
+  });
 }
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
 app.use(express.static(__dirname + '/public'));
 
+router.get('/allBuoys', (req, res) => {
+  res.send("data!");
+});
+
+router.get('/favBuoys', (req, res) => {
+  res.send("datum!");
+});
+
+app.use('/api', router);
 app.listen(process.env.PORT||8000);
