b85c2b7437a3eb183eb96fafae3aea0b84a667e7
Sara <sara@sharif.com>
2016-11-12 12:25:42 -0800

    toggles between all and favorite buoys

--- public/main.js
+++ public/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  console.log("document is ready!");
+  showAllBuoys();
+
+  $("button#showAll").click(function() {
+    showAllBuoys();
+  });
+
+  $("button#showFavs").click(function() {
+    showFavBuoys();
+  });
 });
+
+function showAllBuoys() {
+  $(".allBuoys").removeClass("hidden").html('<h1>....loading buoys within radius...</h1>');
+  $(".favBuoys").addClass("hidden");
+}
+
+function showFavBuoys() {
+  $(".favBuoys").removeClass("hidden").html('<h1>....loading your favorite buoys....</h1>');
+  $(".allBuoys").addClass("hidden");
+}
